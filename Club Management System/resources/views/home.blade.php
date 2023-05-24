fcdec8c4dcfa8702e2a33047a59b2a29e6d61109
AzwadBhuiyan <azwadbhuiyan23@gmail.com>
2023-05-24 20:38:52 +0600

    home demo

--- Club Management System/resources/views/home.blade.php
+++ Club Management System/resources/views/home.blade.php
@@ -1,28 +1,186 @@
+
+
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<html>
+<html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>CMS</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>Laravel</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <script src="{{ asset('js/app.js') }}" defer></script>
+  <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="fonts/icomoon/style.css">
 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
-    </script>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+  <link rel="stylesheet" href="css/magnific-popup.css">
+  <link rel="stylesheet" href="css/jquery-ui.css">
+  <link rel="stylesheet" href="css/owl.carousel.min.css">
+  <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+
+  <link rel="stylesheet" href="css/aos.css">
+
+  <link rel="stylesheet" href="css/style.css">
+
 </head>
 
 <body>
 
-    <a href="/views/approvalProgress.blade.php">Hello!</a>
+  <div class="site-wrap">
 
+
+    <div class="site-navbar py-2">
+
+      <div class="search-wrap">
+        <div class="container">
+          <a href="#" class="search-close js-search-close"><span class="icon-close2"></span></a>
+          <form action="#" method="post">
+            <input type="text" class="form-control" placeholder="Search keyword and hit enter...">
+          </form>
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="d-flex align-items-center justify-content-between">
+          <div class="logo">
+            <div class="site-logo">
+              <a href="index.html">CMS</a>
+            </div>
+          </div>
+          <div class="main-nav d-none d-lg-block">
+            <nav class="site-navigation text-right text-md-center" role="navigation">
+              <ul class="site-menu js-clone-nav d-none d-lg-block">
+                <li><a href="index.html">Home</a></li>
+                <li><a href="shop.html">All medicines</a></li>
+              
+                <li><a href="devices.html">Medical Devices</a></li>
+                <li><a href="test.html">Diagnosis Test</a></li>
+              </ul>
+            </nav>
+          </div>
+          <div class="icons">
+            <a href="#" class="icons-btn d-inline-block js-search-open"><span class="icon-search"></span></a>
+           
+            </a>
+            <a href="#" class="site-menu-toggle js-menu-toggle ml-3 d-inline-block d-lg-none"><span
+                class="icon-menu"></span></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  
+    <div class="site-section bg-success">
+      <div class="container">
+        <div class="row justify-content-center text-center">
+         <div class="col-lg-7">
+           <p class="mb-0"><a href="test.html" class="btn btn-outline-white">Request for diagnosis test</a></p> <br>
+           <p class="mb-0"><a href="test.html" class="btn btn-outline-white">Call an Ambulance</a></p>
+
+         </div>
+        </div>
+      </div>
+    </div>
+    
+    <div class="site-section bg-light">
+      <div class="container">
+        <div class="row">
+          <div class="title-section text-center col-12">
+            <h2>Pharmacy Products</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 block-3 products-wrap">
+            <div class="nonloop-block-3 owl-carousel">
+
+              <div class="text-center item mb-4 item-v2">
+                <a href="shop-single.html"> <img src="images/napa-500.webp" alt="Image" height="300px"></a>
+                <h3 class="text-dark"><a href="shop-single.html">NAPA 500mg</a></h3>
+                <p class="price">10 Tablets</p>
+
+                <p class="price">BDT 12</p>
+              </div>
+
+              <div class="text-center item mb-4 item-v2">
+                <a href="shop-single.html"> <img src="images/fexo-120.webp" alt="Image" height="300px"></a>
+                <h3 class="text-dark"><a href="shop-single.html">Fexo 120mg</a></h3>
+                <p class="price">10 Tablets</p>
+
+                <p class="price">BDT 80</p>
+              </div>
+
+              <div class="text-center item mb-4 item-v2">
+                <a href="shop-single.html"> <img src="images/orsaline-n.webp" alt="Image" height="300px"></a>
+                <h3 class="text-dark"><a href="shop-single.html">Orsaline N</a></h3>
+                <p class="price"> 1 piece </p>
+
+                <p class="price">BDT 5</p>
+              </div>
+
+              <div class="text-center item mb-4 item-v2">
+                <a href="shop-single.html"> <img src="images/ceevit-250mg.webp" alt="Image" height="300px"></a>
+                <h3 class="text-dark"><a href="shop-single.html">Ceevit 250mg</a></h3>
+                <p class="price">10 Tablets</p>
+
+                <p class="price">BDT 19</p>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+ 
+
+  
+    <footer class="site-footer bg-dark">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
+
+            <div class="block-7">
+              <h4 class="mb-4">About</h3>
+
+            </div>
+
+          </div>
+          <div class="col-lg-3 mx-auto mb-5 mb-lg-0">
+            <h4 class="mb-4">Privacy Policy</h3>
+           
+          </div>
+
+          <div class="col-md-6 col-lg-3">
+            <div class="block-5 mb-5">
+              <h4 class=" mb-4">Contact Us</h3>
+              
+            </div>
+
+
+          </div>
+        </div>
+        <div class="row text-center bg-gray">
+          <div class="col-md-12">
+            <p>
+              Copyright &copy;
+              <script>document.write(new Date().getFullYear());</script> All rights reserved | OPDC
+            </p>
+          </div>
+
+        </div>
+      </div>
+    </footer>
+  </div>
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/aos.js"></script>
+
+  <script src="js/main.js"></script>
 
 </body>
 
