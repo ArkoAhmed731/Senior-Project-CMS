39f22a549b72c5844ec3ef38d4193f4ec640a15e
AzwadBhuiyan <azwadbhuiyan23@gmail.com>
2023-05-27 00:48:35 +0600

    home and nav

--- Club Management System/resources/views/home.blade.php
+++ Club Management System/resources/views/home.blade.php
@@ -9,17 +9,17 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="fonts/icomoon/style.css">
+  {{-- <link rel="stylesheet" href="fonts/icomoon/style.css"> --}}
 
   <link rel="stylesheet" href="css/bootstrap.min.css">
-  <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
-  <link rel="stylesheet" href="css/magnific-popup.css">
-  <link rel="stylesheet" href="css/jquery-ui.css">
-  <link rel="stylesheet" href="css/owl.carousel.min.css">
-  <link rel="stylesheet" href="css/owl.theme.default.min.css">
+  {{-- <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css"> --}}
+  {{-- <link rel="stylesheet" href="css/magnific-popup.css"> --}}
+  {{-- <link rel="stylesheet" href="css/jquery-ui.css"> --}}
+  {{-- <link rel="stylesheet" href="css/owl.carousel.min.css"> --}}
+  {{-- <link rel="stylesheet" href="css/owl.theme.default.min.css"> --}}
 
 
-  <link rel="stylesheet" href="css/aos.css">
+  {{-- <link rel="stylesheet" href="css/aos.css"> --}}
 
   <link rel="stylesheet" href="css/style.css">
 
@@ -30,41 +30,28 @@
   <div class="site-wrap">
 
 
-    <div class="site-navbar py-2">
-
-      {{-- <div class="search-wrap">
-        <div class="container">
-          <a href="#" class="search-close js-search-close"><span class="icon-close2"></span></a>
-          <form action="#" method="post">
-            <input type="text" class="form-control" placeholder="Search keyword and hit enter...">
-          </form>
-        </div>
-      </div> --}}
-
-      <div class="container">
-        <div class="d-flex align-items-center justify-content-between">
-          <div class="logo">
-            <div class="site-logo">
-              <a href="index.html">CMS LOGO</a>
-            </div>
-          </div>
-          <div class="main-nav d-none d-lg-block">
-            <nav class="site-navigation text-right text-md-center" role="navigation">
-              <ul class="site-menu js-clone-nav d-none d-lg-block">
-                <li><a href="index.html">Home Icon</a></li>
-                <li><a href="shop.html">Saved Icon</a></li>
-              
-                <li><a href="devices.html">Notification Icon</a></li>
-                <li><a href="test.html">Settings Icon</a></li>
-              </ul>
-            </nav>
-          </div>
-          <div>
-            logout Button
-          </div>
-        </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          
+        </ul>
+        
       </div>
-    </div>
+    </nav>
   {{-- carousel --}}
     <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
         <ol class="carousel-indicators">
@@ -97,68 +84,65 @@
     
     
 {{-- cards --}}
-  <div class="site-section bg-light d-flex justify-content-around">
-        <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
+  <div class="site-section bg-light d-flex align-content-center flex-wrap p-4">
+        <div class="card text-white bg-info m-3 p-3" style="max-width: 18rem;">
             <div class="card-header">Header</div>
             <div class="card-body">
               <h5 class="card-title">Info card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p class="card-text"></p>
             </div>
           </div> 
-         
-          <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
+
+          <div class="card text-white bg-info m-3 p-3" style="max-width: 18rem;">
             <div class="card-header">Header</div>
             <div class="card-body">
               <h5 class="card-title">Info card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p class="card-text"></p>
             </div>
           </div>
-          <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
+         
+          <div class="card text-white bg-info m-3 p-3" style="max-width: 18rem;">
             <div class="card-header">Header</div>
             <div class="card-body">
               <h5 class="card-title">Info card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p class="card-text"></p>
+            </div>
+          </div>
+          <div class="card text-white bg-info m-3 p-3" style="max-width: 18rem;">
+            <div class="card-header">Header</div>
+            <div class="card-body">
+              <h5 class="card-title">Info card title</h5>
+              <p class="card-text"></p>
+            </div>
+          </div>
+          <div class="card text-white bg-info m-3 p-3" style="max-width: 18rem;">
+            <div class="card-header">Header</div>
+            <div class="card-body">
+              <h5 class="card-title">Info card title</h5>
+              <p class="card-text"></p>
+            </div>
+          </div>
+          <div class="card text-white bg-info m-3 p-3" style="max-width: 18rem;">
+            <div class="card-header">Header</div>
+            <div class="card-body">
+              <h5 class="card-title">Info card title</h5>
+              <p class="card-text"></p>
             </div>
           </div>
           
-          <a href="/my-clubs">
-            <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
-                <div class="card-header">My Clubs</div>
-                <div class="card-body">
-                   <h5 class="card-title">Info card title</h5>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                </div>
-              </div>
-          </a>
-        
-    </div>
+  </div>    
+
   
   {{-- footer --}}
     <footer class="site-footer bg-dark">
       <div class="container">
-        <div class="row">
-          <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
-
-            <div class="block-7">
-              <h4 class="mb-4">About</h3>
-
-            </div>
-
-          </div>
-          <div class="col-lg-3 mx-auto mb-5 mb-lg-0">
-            <h4 class="mb-4">Privacy Policy</h3>
-           
-          </div>
-
-          <div class="col-md-6 col-lg-3">
-            <div class="block-5 mb-5">
-              <h4 class=" mb-4">Contact Us</h3>
-              
-            </div>
-
-
-          </div>
+        <div class="d-flex justify-content-center">
+          <div class="p-2"><a href="test.html">About</a></div>
+          <div class="p-2"><a href="test.html">Privacy Policy</a></div>
+          <div class="p-2"><a href="test.html">Contact Us</a></div>
         </div>
+<hr>
+
         <div class="row text-center bg-gray">
           <div class="col-md-12">
             <p>
