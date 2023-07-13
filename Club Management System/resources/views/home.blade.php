2be48c77d17a0ff352161bc36d57ee6febb16c7d
AzwadBhuiyan <azwadbhuiyan23@gmail.com>
2023-07-13 21:59:23 +0600

    test

--- Club Management System/resources/views/home.blade.php
+++ Club Management System/resources/views/home.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>CMS</title>
+    <title>CMSs</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
